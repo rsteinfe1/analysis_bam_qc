02040cfa36182e23766da9d5bd8418ced66309b5
rsteinfe1 <rsteinfe@fredhutch.org>
2022-11-02 13:25:03 -0700

    Aded a qc script

---
+++ qc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ml SAMtools/1.16.1-GCC-11.2.0
+
+bamdir="/fh/scratch/delete90/peters_u/TRPCD/trpcd_data_bam.v1/"
+mkdir flagstat
+
+for bam in $(ls ${bamdir}) ; do
+	samtools flagstat -@ 4 ${bamdir}$bam > flagstat/${bam%.bam}.txt
+done
+
